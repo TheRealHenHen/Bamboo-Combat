93e592f8e1931731f8fe07be7649108945fa5e45
TheRealHenHen <henhenprogrammer@gmail.com>
2022-04-07 00:31:17 +0800

    Reorganized Model and Texture Files

--- src/main/java/net/bamboo/combat/Client.java
+++ src/main/java/net/bamboo/combat/Client.java
@@ -2,24 +2,38 @@ package net.bamboo.combat;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.client.render.entity.model.EntityModelLayer;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
+import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.item.Item;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 import java.util.UUID;
 
 import net.bamboo.combat.entity.SpearEntity;
+import net.bamboo.combat.entity.SpearEntityModel;
+import net.bamboo.combat.entity.SpearEntityModelLayers;
 import net.bamboo.combat.entity.SpearEntityRenderer;
+import net.bamboo.combat.item.SpearItemRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 
 @Environment(EnvType.CLIENT)
 public class Client implements ClientModInitializer {
+	
+    public static final EntityModelLayer SPEAR = new EntityModelLayer(new Identifier(Main.MODID, "spear"), "main");
 
-	public void registerModelPredicate(Item item) {
-		ModelPredicateProviderRegistry.register(item, new Identifier("pulling"), (itemStack, clientWorld, livingEntity, seed) -> {
+	public void registerThrowingPredicate(Item item) {
+		ModelPredicateProviderRegistry.register(item, new Identifier("throwing"), (itemStack, clientWorld, livingEntity, seed) -> {
 			if (livingEntity == null) {
 				return 0.0F;
 			}
@@ -29,16 +43,17 @@ public class Client implements ClientModInitializer {
 
     @Override
 	public void onInitializeClient() {
-
-		EntityRendererRegistry.register(Main.BAMBOO_SPEAR, (context) -> new SpearEntityRenderer(context));
 		
-		registerModelPredicate(Main.Bamboo);
-		registerModelPredicate(Main.Stone);
-		registerModelPredicate(Main.Copper);
-		registerModelPredicate(Main.Iron);
-		registerModelPredicate(Main.Gold);
-		registerModelPredicate(Main.Diamond);
-		registerModelPredicate(Main.Netherite);
+		//EntityRendererRegistry.register(Main.BAMBOO_SPEAR, (context) -> new SpearEntityRenderer(context));
+
+		registerThrowingPredicate(Main.Bamboo);
+		registerThrowingPredicate(Main.Stone);
+		registerThrowingPredicate(Main.Copper);
+		registerThrowingPredicate(Main.Iron);
+		registerThrowingPredicate(Main.Gold);
+		registerThrowingPredicate(Main.Diamond);
+		registerThrowingPredicate(Main.Netherite);
+
 		
 		ClientSidePacketRegistry.INSTANCE.register(new Identifier(Main.MODID, "bamboo_spear"), (context, packet) -> {
 
@@ -48,13 +63,27 @@ public class Client implements ClientModInitializer {
 
 			int entityID = packet.readInt();
 			UUID entityUUID = packet.readUuid();
+			MinecraftClient mc = MinecraftClient.getInstance();
 
 			context.getTaskQueue().execute(() -> {
-				SpearEntity spear = new SpearEntity(MinecraftClient.getInstance().world, x, y, z, entityID, entityUUID);
-				MinecraftClient.getInstance().world.addEntity(entityID, spear);
+				SpearEntity spear = new SpearEntity(mc.world, x, y, z, entityID, entityUUID);
+				mc.world.addEntity(entityID, spear);
 			});
 
 		});
+
+		Item item = Main.Bamboo;
+		Identifier spearId = Registry.ITEM.getId(item);
+		Identifier texture = new Identifier(spearId.getNamespace(), "textures/entity/" + spearId.getPath() + ".png");
+
+		EntityModelLayer modelLayer =  EntityModelLayers.TRIDENT;
+		SpearItemRenderer spearItemRenderer = new SpearItemRenderer(spearId, texture, modelLayer);
+		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(spearItemRenderer);
+		BuiltinItemRendererRegistry.INSTANCE.register(item, spearItemRenderer);
+		ModelLoadingRegistry.INSTANCE.registerModelProvider((manager, out) -> out.accept(new ModelIdentifier(spearId + "_in_inventory", "inventory")));
+		
+        EntityModelLayerRegistry.registerModelLayer(SpearEntityModelLayers.BAMBOO_SPEAR, SpearEntityModel::getTexturedModelData);
+		EntityRendererRegistry.register(Main.BAMBOO_SPEAR, (context) -> new SpearEntityRenderer(context, new Identifier(Main.MODID, "textures/entity/bamboo_spear/normal.png"), SpearEntityModelLayers.BAMBOO_SPEAR));
 
     }
 	
