56b8a35bed7d7076fb05bea38fe9c7a1b5264ec3
TheRealHenHen <henhenprogrammer@gmail.com>
2022-04-11 12:47:10 +0800

    Removed models

--- src/main/java/net/bamboo/combat/entity/SpearEntityModel.java
+++ src/main/java/net/bamboo/combat/entity/SpearEntityModel.java
@@ -32,28 +32,8 @@ public class SpearEntityModel extends Model {
         modelPartData2.addChild("top_spike", ModelPartBuilder.create().uv(9, 0).cuboid(-0.5F, -4.0F, 0.1F, 1.0F, 0.5F, 0.4F), ModelTransform.NONE);
         return TexturedModelData.of(modelData, 32, 32);
     }
-    
-    public static TexturedModelData modelBambooDamaged() {
-        ModelData modelData = new ModelData();
-        ModelPartData modelPartData = modelData.getRoot();
-        ModelPartData modelPartData2 =
-        modelPartData.addChild("pole", ModelPartBuilder.create().uv(0, -1).cuboid(-0.5F, -3.0F, -0.5F, 1.0F, 22.0F, 1.0F), ModelTransform.NONE);
-        modelPartData2.addChild("bottom_spike", ModelPartBuilder.create().uv(5, 0).cuboid(-0.5F, -3.5F, -0.2F, 1.0F, 0.5F, 0.7F), ModelTransform.NONE);
-        modelPartData2.addChild("top_spike", ModelPartBuilder.create().uv(9, 0).cuboid(-0.5F, -4.0F, 0.1F, 1.0F, 0.5F, 0.4F), ModelTransform.NONE);
-        return TexturedModelData.of(modelData, 32, 32);
-    }
 
     public static TexturedModelData modelStone() {
-        ModelData modelData = new ModelData();
-        ModelPartData modelPartData = modelData.getRoot();
-        ModelPartData modelPartData2 =
-        modelPartData.addChild("pole", ModelPartBuilder.create().uv(0, -1).cuboid(-0.5F, -2.0F, -0.5F, 1.0F, 28.0F, 1.0F), ModelTransform.NONE);
-        modelPartData2.addChild("bottom_stone", ModelPartBuilder.create().uv(5, 0).cuboid(-1.0F, -4.0F, -1.0F, 2.0F, 3.0F, 2.0F), ModelTransform.NONE);
-        modelPartData2.addChild("top_stone", ModelPartBuilder.create().uv(15, 1).cuboid(-0.5F, -5.0F, -0.5F, 1.0F, 1.0F, 1.0F), ModelTransform.NONE);
-        return TexturedModelData.of(modelData, 32, 32);
-    }
-
-    public static TexturedModelData modelStoneDamaged() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
         ModelPartData modelPartData2 =
