3e6ec00ee5e844be24cf3556f66d21961dab265e
TheRealHenHen <henhenprogrammer@gmail.com>
2022-05-22 14:16:19 +0800

    Removed unnecessary code

--- src/main/java/net/bamboo/combat/BambooCombatClient.java
+++ src/main/java/net/bamboo/combat/BambooCombatClient.java
@@ -4,7 +4,6 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.util.ModelIdentifier;
-import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -16,28 +15,22 @@ import net.bamboo.combat.entity.SpearEntityModelLayers;
 import net.bamboo.combat.entity.SpearEntityRenderer;
 import net.bamboo.combat.item.BambooItems;
 import net.bamboo.combat.item.spear.SpearItem;
-import net.bamboo.combat.item.spear.SpearItemRenderer;
+//import net.bamboo.combat.item.spear.SpearItemRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry.TexturedModelDataProvider;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 
 @Environment(EnvType.CLIENT)
 public class BambooCombatClient implements ClientModInitializer {
 
 	private void register(SpearItem item, EntityModelLayer modelLayer, TexturedModelDataProvider provider) {
 		Identifier spearId = Registry.ITEM.getId(item);
-		Identifier texture = new Identifier(spearId.getNamespace(), "textures/entity/" + spearId.getPath() + "/normal.png");
 
-		SpearItemRenderer spearItemRenderer = new SpearItemRenderer(spearId, texture, modelLayer);
-		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(spearItemRenderer);
-		BuiltinItemRendererRegistry.INSTANCE.register(item, spearItemRenderer);
 		ModelLoadingRegistry.INSTANCE.registerModelProvider((manager, out) -> out.accept(new ModelIdentifier(spearId + "/" + "gui", "inventory")));
 		
         EntityModelLayerRegistry.registerModelLayer(modelLayer, provider);
