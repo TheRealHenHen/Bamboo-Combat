49450a656bde6e07135dffe7e19d410e6585d9ec
TheRealHenHen <henhenprogrammer@gmail.com>
2023-07-14 21:45:00 +0800

    Rearranged spears

--- src/main/java/net/bamboo/combat/entity/spear/SpearEntityTypes.java
+++ src/main/java/net/bamboo/combat/entity/spear/SpearEntityTypes.java
@@ -23,8 +23,8 @@ public class SpearEntityTypes {
 
     public static final EntityType<SpearEntity> BAMBOO_SPEAR = registerEntity("bamboo_spear");
     public static final EntityType<SpearEntity> STONE_BAMBOO_SPEAR = registerEntity("stone_bamboo_spear");
-    public static final EntityType<SpearEntity> COPPER_BAMBOO_SPEAR = registerEntity("copper_bamboo_spear");
     public static final EntityType<SpearEntity> IRON_BAMBOO_SPEAR = registerEntity("iron_bamboo_spear");
+    public static final EntityType<SpearEntity> COPPER_BAMBOO_SPEAR = registerEntity("copper_bamboo_spear");
     public static final EntityType<SpearEntity> GOLDEN_BAMBOO_SPEAR = registerEntity("golden_bamboo_spear");
     public static final EntityType<SpearEntity> DIAMOND_BAMBOO_SPEAR = registerEntity("diamond_bamboo_spear");
     public static final EntityType<SpearEntity> NETHERITE_BAMBOO_SPEAR = registerEntity("netherite_bamboo_spear");
