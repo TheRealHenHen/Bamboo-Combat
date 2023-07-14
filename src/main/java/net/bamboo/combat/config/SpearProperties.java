9f8fe48f392816199dcd9c50081a1b4f72fe049a
TheRealHenHen <henhenprogrammer@gmail.com>
2023-07-14 12:33:09 +0800

    Made SpearProperties its own file

---
+++ src/main/java/net/bamboo/combat/config/SpearProperties.java
@@ -0,0 +1,16 @@
+package net.bamboo.combat.config;
+
+public class SpearProperties {
+        public boolean canCriticalThrow;
+        public boolean canPierce;
+        public int durability;
+        public int attackDamage;
+        public float attackSpeed;
+        public float throwDistance;
+        public float dragInWater;
+        public int throwDelay;
+        public int pierceLevel;
+        public int burnTicks;
+        public int durabilityDecreaseAfterThrown;
+        public int throwDamageDecreaseAfterPierce;
+    }
