24420e002b57f2f92a939426c71cdd982a72b75c
TheRealHenHen <henhenprogrammer@gmail.com>
2024-07-20 22:29:04 +0800

    Made packet initialization into a method

--- src/main/java/net/bamboo/combat/BambooCombat.java
+++ src/main/java/net/bamboo/combat/BambooCombat.java
@@ -2,6 +2,7 @@ package net.bamboo.combat; //By TheRealHenHen
 
 import net.bamboo.combat.config.Config;
 import net.bamboo.combat.config.ConfigFile;
+import net.bamboo.combat.entity.spear.SpearEntityPacket;
 import net.bamboo.combat.item.BambooItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -15,6 +16,7 @@ public class BambooCombat implements ModInitializer{
         ConfigFile.initialize();
         config = ConfigFile.INSTANCE;
         BambooItems.initialize();
+        SpearEntityPacket.initializePacket();
     }
-
+    
 }
